2946200b7ab50a3d13dc89f754ed243d151e0aba
nicoleisbusy <60363959+nicoleisbusy@users.noreply.github.com>
2020-03-04 14:20:44 +0100

    arbejder på values section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 console.log("it works");
+document.getElementById("kundebtn").onclick = function() {slideInKunde()};
+function slideInKunde() {
+  var win = document.getElementByClass("kundeslide");
+  win.style.transition = "right 1s ease-in-out 0s";
+	win.style.left = "0px";
+}
