56de46bf7387eb0eb13de0cf692c35082b800c9b
nicoleisbusy <60363959+nicoleisbusy@users.noreply.github.com>
2020-03-08 18:58:33 +0100

    produkt slider

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-console.log("it works");
-document.getElementById("kundebtn").onclick = function() {slideInKunde()};
-function slideInKunde() {
-  var win = document.getElementByClass("kundeslide");
-  win.style.transition = "right 1s ease-in-out 0s";
-	win.style.left = "0px";
+let slideIndex = 1;
+
+const showSlides = (n) => {
+
+  const slides = document.getElementsByClassName("imageslide");
+  console.log("number of slides is:", slides.length);
+
+  if( n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if( n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for(let i=0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display ="block";
+  console.log("my slide position is:", slideIndex - 1);
 }
+
+const plusSlides = (n) => {
+  showSlides( slideIndex = slideIndex + n);
+};
+
+showSlides(slideIndex);
